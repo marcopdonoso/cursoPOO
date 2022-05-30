2dea805b5522b62114385affbff5afe9f0e068f7
marcopdonoso <marcopdonoso@gmail.com>
2022-05-30 16:51:18 -0400

    Instanciamos objetos de la clase Car y les dimos atributos, esto a manera de ejemplo con Java y Python

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,15 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundoxx");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passengers = 4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "QWE567";
+        car2.driver = "Andrea Herrera";
+        car2.passengers = 3;
+        car2.printDataCar();
     }
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,12 @@
+from car import Car
+
 if __name__ == "__main__":
-    print("Hola mundo")
+    car = Car()
+    car.license = "AMS123"
+    car.driver = "Willy"
+    print(vars(car))
+    
+    car2 = Car()
+    car2.license = "GRS983"
+    car2.driver = "Drogo"
+    print(vars(car2))
