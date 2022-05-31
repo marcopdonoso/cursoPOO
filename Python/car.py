b18523f5680c02258ddabe8534ff64615cca0848
marcopdonoso <marcopdonoso@gmail.com>
2022-05-31 14:44:43 -0400

    Declaré un método constructor para las clases Account y Car en Pyhton con __init__ y luego añadí el código en main.py para instanciar el objeto car

--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,15 @@
+from lib2to3.pgen2 import driver
+from account import Account
+
+
+from account import Account
+
 class Car:
     id          = int
     license     = str
-    driver      = str
+    driver      = Account("","")
     passengers  = str
+    
+    def __init__(self, license, driver):
+        self.license    = license
+        self.driver     = driver
--- Python/main.py
+++ Python/main.py
@@ -1,12 +1,7 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
-    car = Car()
-    car.license = "AMS123"
-    car.driver = "Willy"
+    car = Car("AMS234",Account("Willy","ANDA876"))
     print(vars(car))
-    
-    car2 = Car()
-    car2.license = "GRS983"
-    car2.driver = "Drogo"
-    print(vars(car2))
+    print(vars(car.driver))
