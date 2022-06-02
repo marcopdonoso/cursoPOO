4c650a8aac8fd311ff82dc9b4afb74c6e1dfb55e
marcopdonoso <marcopdonoso@gmail.com>
2022-06-02 14:53:54 -0400

    Creo las clases Card, Cash y Paypal en PHP, haciendo uso de la herencia

---
+++ PHP/paypal.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'payment.php';
+class PayPal extends Payment {
+    public $email;
+
+    public function __construct($id, $email) {
+        parent::__construct($id);
+        $this->email = $email;
+    }
+}
